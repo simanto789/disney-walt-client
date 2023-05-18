18df2bf12256507fcb92affd51c607e7c4ae1e1d
simanto789 <hazrat.ali789@gmail.com>
2023-05-18 14:24:07 +0600

    add footer

--- src/LayOut/Main.jsx
+++ src/LayOut/Main.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
  
 
 
@@ -6,6 +7,7 @@ const Main = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
